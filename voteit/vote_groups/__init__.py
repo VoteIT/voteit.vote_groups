e0c326831f89c4af9bd9bfbbad8578b350606f80
Robin Harms Oredsson <robin@betahaus.net>
2018-02-22 12:16:45 +0100

    Adding locale calatalog

--- voteit/vote_groups/__init__.py
+++ voteit/vote_groups/__init__.py
@@ -3,11 +3,12 @@ from __future__ import unicode_literals
 
 from pyramid.i18n import TranslationStringFactory
 
-PROJECTNAME = 'voteit.vote_groups'
-_ = TranslationStringFactory(PROJECTNAME)
+
+_ = TranslationStringFactory('voteit.vote_groups')
 
 
 def includeme(config):
     config.include('.views')
     config.include('.models')
     config.include('.schemas')
+    config.add_translation_dirs('voteit.vote_groups:locale/')
